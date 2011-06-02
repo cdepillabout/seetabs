fb0ee6bef7694acdd60cef10b417ee55a910878a
Dennis Gosnell <cdep.illabout@gmail.com>
2011-06-02 19:13:25 -0400

    Initial commit.
    These files have been created with the add-on builder:
    https://addons.mozilla.org/en-US/developers/tools/builder

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.seetabs.boolpref", false);
+pref("extensions.seetabs.intpref", 0);
+pref("extensions.seetabs.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.cdep.illabout@gmail.com.description", "chrome://seetabs/locale/overlay.properties");
