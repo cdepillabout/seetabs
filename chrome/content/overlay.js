058e618376c871370a86d2bf8e224d5c1511302f
(cdep) illabout <cdep.illabout@gmail.com>
2011-08-12 22:16:37 -0400

    Did some more tests. Found out that you can not get the old location of a tab when it was moved.... :(

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,6 +1,6 @@
 
 // utility functions that are used by seetabs
-var ntutils = {
+var stutils = {
 
 	// Return the string representation of list, but with good formatting.
 	// Only print numitems per line.
@@ -19,13 +19,31 @@ var ntutils = {
 	},
 
 	// return values in object
-	dir: function(object) {
-		stuff = [];
-		for (s in object) {
-			stuff.push(s);
+	xinspect: function (object, depth_index) {
+		if (typeof depth_index == 'undefined') {
+			depth_index='';
 		}
-		stuff.sort();
-		return stuff;
+		//alert("depth_index.length = " + depth_index.length + "\"" + depth_index + "\"");
+
+		if (depth_index.length > 0) {
+			return '[MAX ITERATIONS]';
+		}
+
+		var result = [];
+		for(var prop in object) {
+			var prop_type = typeof object[prop];
+
+			var temp = "";
+			if (prop_type == 'object') {
+				temp = 'object: ' + this.xinspect(object[prop], depth_index + '  ');
+			}
+			else {
+				temp = object[prop] + '';
+			}
+
+			result.push(depth_index + '"' + prop + '" (' + prop_type + ') => ' + temp);
+		}
+		return result.join(depth_index + '\n');
 	},
 
 	// Open a url in a new tab. Optionally make it active.
@@ -83,12 +101,16 @@ var ntutils = {
 
 		return "";
 	},
+
+	log: function(string) {
+		dump("SEETABS: " + string + "\n");
+	},
 }
 
 var seetabs = {
 
 	// cookie for seetabs
-	cookie: null, 
+	cookie: null,
 
 	// has this been initialized yet?
 	initialized: false,
@@ -96,22 +118,25 @@ var seetabs = {
 	// hostname to use when sending history to website
 	host: "http://seetabs.com/",
 
+	log: stutils.log,
+
 	init: function() {
+		this.log("asefasefasef");
 		if (this.initialized) {
 			return;
 		}
 
 		// get the cookie from seetabs
-		this.cookie = ntutils.getcookie("seetabs.com", "client_token");
+		//this.cookie = stutils.getcookie("seetabs.com", "client_token");
 		if (!this.cookie) {
-			ntutils.browse("http://seetabs.com/newuser.php", true);
+			//stutils.browse("http://seetabs.com/newuser.php", true);
 		}
 
 		if (gBrowser) {
 			//gBrowser.addEventListener("DOMContentLoaded", this.onPageLoad, false);
 
 			var container = gBrowser.tabContainer;
-			container.addEventListener("TabOpen", this.onTabOpen, false);
+			container.addEventListener("TabOpen", seetabs.onTabOpen, false);
 			container.addEventListener("TabMove", this.onTabMove, false);
 			container.addEventListener("TabClose", this.onTabClose, false);
 		}
@@ -153,9 +178,9 @@ var seetabs = {
 
 	onPageLoad: function(aEvent) {
 		// document that triggered the event
-		var doc = aEvent.originalTarget; 
+		var doc = aEvent.originalTarget;
 		// window for the doc
-		var win = doc.defaultView; 
+		var win = doc.defaultView;
 
 		if (doc.nodeName != "#document") return; // only documents
 		if (win != win.top) return; //only top window.
@@ -163,14 +188,14 @@ var seetabs = {
 
 		var tabindex = gBrowser.getBrowserIndexForDocument(doc);
 
-		seetabs.debuglala(doc.location.href, doc.title, ntutils.getdesc(doc), 
+		seetabs.debuglala(doc.location.href, doc.title, stutils.getdesc(doc),
 						  seetabs.cookie, tabindex, "onPageLoad");
 		return;
 
-		// firefox will open file://, chrome://, and about: pages, but we don't 
+		// firefox will open file://, chrome://, and about: pages, but we don't
 		// want to save those in our history.  These pages also won't have a description.
 		if (doc.location.protocol == "http:" || doc.location.protocol == "https:") {
-			seetabs.recordVisit(doc.location.href, doc.title, ntutils.getdesc(doc), seetabs.cookie);
+			seetabs.recordVisit(doc.location.href, doc.title, stutils.getdesc(doc), seetabs.cookie);
 		}
 	},
 
@@ -179,6 +204,9 @@ var seetabs = {
 	},
 
 	onTabMove: function(aEvent) {
+		// NOTE: There doesn't seem like there is any way to get where the tab
+		// came from, so we will probably have to send all tabs to the server
+		// when a tab move event occurs.
 		seetabs.onTabDebug(aEvent, "onTabMove");
 	},
 
@@ -187,28 +215,39 @@ var seetabs = {
 	},
 
 	onTabDebug: function(aEvent, from) {
+		this.log("from " + from + ": " + "aEvent=(" + aEvent + ")\n" +
+				stutils.xinspect(aEvent));
 		var tab = aEvent.target;
+		var old_tab = aEvent.originalTarget;
+		seetabs.onTabDebugHelper(from, tab, old_tab);
+	},
+
+	onTabDebugHelper: function(from, tab, old_tab) {
 		var browser = gBrowser.getBrowserForTab(tab);
 		var doc = browser.contentDocument;
-		var win = doc.defaultView; 
+		var win = doc.defaultView;
 
-		//if (doc.nodeName != "#document") return; // only documents
-		//if (win != win.top) return; //only top window.
-		//if (win.frameElement) return; // skip iframes/frames
+		var old_browser = gBrowser.getBrowserForTab(old_tab);
+		var old_doc = old_browser.contentDocument;
+		var old_win = old_doc.defaultView;
+
+		if (doc.nodeName != "#document") return; // only documents
+		if (win != win.top) return; //only top window.
+		if (win.frameElement) return; // skip iframes/frames
 
 		var tabindex = gBrowser.getBrowserIndexForDocument(doc);
+		var old_tabindex = gBrowser.getBrowserIndexForDocument(old_doc);
 		//var tabindex = tab.tabIndex;
 
-		seetabs.debuglala(doc.location.href, doc.title, ntutils.getdesc(doc), 
-						  seetabs.cookie, tabindex, from);
-		return;
+		seetabs.debuglala(doc.location.href, doc.title, stutils.getdesc(doc),
+						  seetabs.cookie, tabindex, old_tabindex, from);
 	},
 
 	recordVisit: function(url, title, description, cookie) {
-		//alert("Recording visit: " + encodeURI(url) + ", title: " + escape(title) + 
+		//alert("Recording visit: " + encodeURI(url) + ", title: " + escape(title) +
 		//	 ", description: " + description);
 		if (!cookie) {
-			seetabs.cookie = ntutils.getcookie("seetabs.com", "client_token");
+			seetabs.cookie = stutils.getcookie("seetabs.com", "client_token");
 			cookie = seetabs.cookie;
 		}
 		var req = new XMLHttpRequest();
@@ -224,9 +263,10 @@ var seetabs = {
 		req.send(null);
 	},
 
-	debuglala: function(url, title, description, cookie, tabindex, from) {
-		alert("debug: " + url + ", title: " + title + ", description: " + description +
-			  ", tabindex: " + tabindex + ", from: " + from);
+	debuglala: function(url, title, description, cookie, tabindex, old_tabindex, from) {
+		this.log("debug: " + url + ", title: " + title + ", description: " + description +
+			  ", tabindex: " + tabindex + ", old_tabindex: " + old_tabindex +
+			  ", from: " + from);
 	},
 };
 
